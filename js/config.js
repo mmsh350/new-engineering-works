3d3c75b5af5ec689f094f0af9224a30a8487bbf0
Muhammad S. Hamidu <mhamidu@swglobal.local>
2024-12-06 17:01:55 +0100

    feat: About us page

--- js/config.js
+++ js/config.js
@@ -5,24 +5,62 @@ window.onload = function () {
     }, 1000);
 };
 
-// script.js
 document.addEventListener("DOMContentLoaded", () => {
+    // Load navigation
     const navContainer = document.getElementById("navigation");
+    if (navContainer) {
+        fetch("includes/navigation.html")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch navigation: ${response.statusText}`);
+                }
+                return response.text();
+            })
+            .then((html) => {
+                navContainer.innerHTML = html;
+            })
+            .catch((error) => {
+                console.error("Error loading navigation:", error);
+            });
+    }
 
-    // Fetch the navigation HTML
-    fetch("includes/navigation.html")
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`Failed to fetch navigation: ${response.statusText}`);
-            }
-            return response.text();
-        })
-        .then((html) => {
-            // Inject the navigation HTML into the container
-            navContainer.innerHTML = html;
-        })
-        .catch((error) => {
-            console.error("Error loading navigation:", error);
-        });
+    // Load footer
+    const footerContainer = document.getElementById("footer-main");
+    if (footerContainer) {
+        fetch("includes/footer-main.html")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch footer: ${response.statusText}`);
+                }
+                return response.text();
+            })
+            .then((html) => {
+                footerContainer.innerHTML = html;
+            })
+            .catch((error) => {
+                console.error("Error loading footer:", error);
+            });
+    }
+
+    // Load copyright 
+    const copyrightContainer = document.getElementById("site-copyright");
+    if (copyrightContainer) {
+        fetch("includes/site-copyright.html")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch footer: ${response.statusText}`);
+                }
+                return response.text();
+            })
+            .then((html) => {
+                copyrightContainer.innerHTML = html;
+            })
+            .catch((error) => {
+                console.error("Error loading footer:", error);
+            });
+    }
+
+
+
 });
 
